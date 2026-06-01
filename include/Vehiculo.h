08fc596c73db6770a2923d9d9944bf8bd9c041b1
DevLucasPr <lucas.saracho010@gmail.com>
2026-06-01 17:42:14 -0300

    Vehiculo.h

--- include/Vehiculo.h
+++ include/Vehiculo.h
@@ -2,19 +2,29 @@
 #define VEHICULO_H
 
 #include "TipoVehiculo.h"
+#include "DTVehiculosConductor.h"
+#include "DTListarViaje.h"
 #include <string>
+#include <set>
+using namespace std;
 
 class Vehiculo {
 private:
-    std::string matricula;
+    string matricula;
     int capacidad;
-    std::string marca;
-    std::string modelo;
+    string marca;
+    string modelo;
     TipoVehiculo tipo;
 
 public:
-    Vehiculo(std::string matricula, int capacidad, std::string marca, std::string modelo, TipoVehiculo tipo);
+    Vehiculo(string matricula, int capacidad, string marca, string modelo, TipoVehiculo tipo);
     ~Vehiculo();
+    set<DTListarViaje> listarViajesVehiculo();
+    DTVehiculosConductor getDTVehiculoConductor();
+    int getCapacidad();
+    bool hayViajesConductor(DTFecha fecha);
+    bool hayViajesFecha(DTFecha fecha);
+    void asociarViaje();
 };
 
 #endif
