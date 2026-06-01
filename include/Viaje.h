4ac3d283d4ccd74377c1df78e23fa9eb07cf8fa8
DevLucasPr <lucas.saracho010@gmail.com>
2026-06-01 17:56:33 -0300

    Viaje.h y cambio en TipoUsuario a enum class por error de sintaxis entre clase y enum

--- include/Viaje.h
+++ include/Viaje.h
@@ -2,20 +2,31 @@
 #define VIAJE_H
 
 #include "DTFecha.h"
+#include "DTUsuarioViaje.h"
+#include "DTConsultaViaje.h"
+#include "Pasajero.h"
 #include <string>
+#include <set>
+using namespace std;
 
 class Viaje {
 private:
     int codigo;
     DTFecha fecha;
-    std::string origen;
-    std::string destino;
+    string origen;
+    string destino;
     int asientosPublicados;
     float precio;
 
 public:
-    Viaje(int codigo, DTFecha fecha, std::string origen, std::string destino, int asientosPublicados, float precio);
+    Viaje(int codigo, DTFecha fecha, string origen, string destino, int asientosPublicados, float precio);
     ~Viaje();
+    set<DTUsuarioViaje> listarUsuariosViaje(int codigo);
+    DTFecha getFecha();
+    bool filtroViaje(DTFecha fecha, string origen, string destino);
+    bool asientosDisp(int asientos);
+    DTConsultaViaje obtenerViajes();
+    bool sePuedeReservar(Pasajero *p, int asientos);
 };
 
 #endif
