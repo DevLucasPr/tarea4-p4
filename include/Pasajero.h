268aaaaf3b5c490bdee809c755efb30fd8088293
Mauricio Mendizábal <mmendizabalriano@gmail.com>
2026-06-02 18:47:36 -0300

    Usuario.h y Pasajero.h

--- include/Pasajero.h
+++ include/Pasajero.h
@@ -3,14 +3,19 @@
 
 #include "Usuario.h"
 #include <string>
+#include <set>
 
 class Pasajero : public Usuario {
 private:
     std::string ci;
 
 public:
-    Pasajero(std::string nickname, std::string nombre, std::string contrasena, std::string email, std::string documento);
+    Pasajero(std::string nickname, std::string nombre, std::string contrasena, std::string email, std::string ci);
     ~Pasajero();
+
+    std::set<DTListarViaje*> listarViajesUsuario();
+    bool esPasajero();
+    void linkReserva();
 };
 
 #endif
--- include/Usuario.h
+++ include/Usuario.h
@@ -2,6 +2,9 @@
 #define USUARIO_H
 
 #include <string>
+#include <set>
+#include "DTListarViaje.h"
+#include "DTFecha.h
 
 class Usuario {
 protected:
@@ -9,10 +12,21 @@ protected:
     std::string nombre;
     std::string contrasena;
     std::string email;
+    float calificacionPromedio;
 
 public:
     Usuario(std::string nickname, std::string nombre, std::string contrasena, std::string email);
     virtual ~Usuario();
+
+    std::string getNickname();
+    std::string getNombre();
+
+    virtual std::set<DTListarViaje*> listarViajesUsuario() = 0;
+    virtual bool esPasajero() = 0;
+
+    bool existeCalificacion(std::string nicknameCalificado, int codigoMem);
+    void calificarUsuario(int puntaje, DTFecha fechaActual);
+    void linkCalifica();
 };
 
 #endif
