bd3e485bee57a250dda12e5138dd61abb394a340
aguslato <agus.latorre1909@gmail.com>
2026-06-11 15:09:00 -0300

    Update Calificacion.cpp

--- src/Calificacion.cpp
+++ src/Calificacion.cpp
@@ -6,3 +6,24 @@ Calificacion::Calificacion(DTFecha fecha, int puntaje) {
 }
 
 Calificacion::~Calificacion() {}
+
+bool Calificacion::existeCalificacion(std::string nicknameCalificado, int codigoMem){
+    
+    bool existe = false;
+    if(usuarioCalificado != nullptr){
+        std:: string nickname = usuarioCalificado->getNickname();
+
+        if((reservaAsociada != nullptr)&&(nickname==nicknameCalificado)){
+            existe= reservaAsociada->estaAsociadoViajeACalif(codigoMem);
+        }
+    }
+    return existe;
+}
+
+void Calificacion::linkCalifica(Usuario usuarioCalificado){
+    this->usuarioCalificado= usuarioCalificado;
+}
+
+void Calificacion::linkReserva(Reserva reservaAsociada){
+    this->reservaAsociada= reservaAsociada;
+}
