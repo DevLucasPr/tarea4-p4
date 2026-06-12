923ce8b84cfd0a5a1a90cec8e5d9f9dd3fec2107
jeydeleon <jey.deleon@eroninternational.com>
2026-06-11 22:44:09 -0300

    implementar Conductor con gestión de vehículos y listado de viajes

--- include/Conductor.h
+++ include/Conductor.h
@@ -3,15 +3,28 @@
 
 #include "Usuario.h"
 #include "TipoLibreta.h"
+#include "TipoVehiculo.h"
+#include "DTVehiculosConductor.h"
+#include "DTFecha.h"
 #include <set>
+
+class Vehiculo;
 
 class Conductor : public Usuario {
 private:
     std::set<TipoLibreta> libretas;
+    std::set<Vehiculo*> vehiculos;
 
 public:
     Conductor(std::string nickname, std::string nombre, std::string contrasena, std::string email, std::set<TipoLibreta> libs);
     ~Conductor();
+
+    std::set<DTListarViaje*> listarViajesUsuario();
+    bool puedeManejarTipo(TipoVehiculo tipo);
+    bool esPasajero();
+    void linkVehiculo(Vehiculo* v);
+    std::set<DTVehiculosConductor> listarVehiculos();
+    bool hayViajesFecha(DTFecha fecha);
 };
 
 #endif
