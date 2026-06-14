5e895f60f1bf940be49051356310df29983b211b
DevLucasPr <lucas.saracho010@gmail.com>
2026-06-14 16:46:44 -0300

    carga datos SIN errores

--- include/ControladorUsuarios.h
+++ include/ControladorUsuarios.h
@@ -1,158 +1,49 @@
-#include "../include/ControladorUsuarios.h"
+#ifndef CONTROLADORUSUARIOS_H
+#define CONTROLADORUSUARIOS_H
 
-#include "../include/Conductor.h"
-#include "../include/Pasajero.h"
-#include "../include/Calificacion.h"
-#include "../include/DTVehiculosConductor.h"
-#include "../include/DTListarViaje.h"
-#include "../include/ControladorViajes.h"
-#include "../include/Fabrica.h"
+#include "IControladorUsuarios.h"
+#include <set>
+#include <string>
+using namespace std;
 
-ControladorUsuarios* ControladorUsuarios::instancia = nullptr;
+#include "TipoLibreta.h"
+#include "TipoVehiculo.h"
 
-ControladorUsuarios::ControladorUsuarios() {
-    codigoMem = 0;
-    nicknameMem = "";
-}
+class DTVehiculosConductor;
+class DTListarViaje;
+class DTUsuario;
+class HandlerVehiculos;
+class HandlerUsuarios;
+class HandlerViajes;
 
-ControladorUsuarios::~ControladorUsuarios() {}
+class ControladorUsuarios : public IControladorUsuarios {
+private:
+	int codigoMem;
+	string nicknameMem;
+	static ControladorUsuarios* instancia;
 
-ControladorUsuarios* ControladorUsuarios::getInstance() {
-    if (instancia == nullptr) {
-        instancia = new ControladorUsuarios();
-    }
-    return instancia;
-}
+	HandlerVehiculos *handlerVehiculos;
+    HandlerUsuarios *handlerUsuarios;
+	HandlerViajes *handlerViajes;
 
-// Crear un nuevo pasajero con los datos proporcionados. Notifica en caso de éxito
-// o de error al crear el pasajero.
-bool ControladorUsuarios::altaPasajero(std::string nickname, std::string nombre, std::string contrasena, std::string email, std::string ci) {
-    if (HandlerUsuarios::getInstance()->existeUsuario(nickname)) {
-        return false;
-    }
-    Pasajero* pasajero = new Pasajero(nickname, nombre, contrasena, email, ci);
-    HandlerUsuarios::getInstance()->agregarPasajero(pasajero);
-    return true;
-}
+	ControladorUsuarios();
 
-// Crear un nuevo conductor con los datos proporcionados. Notifica en caso de
-// éxito o de error al crear el conductor.
-bool ControladorUsuarios::altaConductor(std::string nickname, std::string nombre, std::string contrasena, std::string email, std::set<TipoLibreta> libretas) {
-    if (HandlerUsuarios::getInstance()->existeUsuario(nickname)) {
-        return false;
-    }
-    Conductor* conductor = new Conductor(nickname, nombre, contrasena, email, libretas);
-    HandlerUsuarios::getInstance()->agregarConductor(conductor);
-    return true;
-}
+public:
+	~ControladorUsuarios();
 
-// Devuelve un conjunto de DTVehiculosConductor con los datos de los vehículos del conductor identificado por nickname.
-std::set<DTVehiculosConductor> ControladorUsuarios::listarVehiculosConductor(std::string nickname) {
-    std::set<DTVehiculosConductor> dtVehiculos;
-    Conductor* conductor = HandlerUsuarios::getInstance()->getConductor(nickname);
-    if (conductor != nullptr) {
-        dtVehiculos = conductor->listarVehiculos();
-    }
-    return dtVehiculos;
-}
+	bool altaPasajero(string nickname, string nombre, string contrasena, string email, string ci);
+	bool altaConductor(string nickname, string nombre, string contrasena, string email, set<TipoLibreta> libretas);
+	set<DTVehiculosConductor> listarVehiculosConductor(string nickname);
+	int registrarVehiculo(string nickname, string matricula, int capacidad, string marca, string modelo, TipoVehiculo tipo);
+	set<DTUsuario> listarUsuarios();
+	set<DTListarViaje> listarViajes(string nickname);
+	set<string> listarPasajeros();
+	bool calificarUsuario(string nicknameCalificado, int calificacion);
 
-// int indicando si la operación fue exitosa o que error ocurrió (0 correcta, -1
-// matrícula repetida, -2 no tiene tipo de licencia para el vehículo) 
-// PRE: Existe conductor
-int ControladorUsuarios::registrarVehiculo(std::string nickname, std::string matricula, int capacidad, std::string marca, std::string modelo, TipoVehiculo tipo) {
-    // matricula repetida -> la coleccion de vehiculos la maneja HandlerVehiculos
-    if (HandlerVehiculos::getInstance()->existeVehiculo(matricula)) {
-        return -1;
-    }
-    Conductor* conductor = HandlerUsuarios::getInstance()->getConductor(nickname);
-    // el conductor es el Expert sobre sus libretas
-    if (!conductor->puedeManejarTipo(tipo)) {
-        return -2;
-    }
-    // HandlerVehiculos crea el vehiculo y lo agrega a la coleccion
-    Vehiculo* vehiculo = HandlerVehiculos::getInstance()->crearVehiculo(matricula, capacidad, marca, modelo, tipo);
-    // link Conductor <-> Vehiculo en ambos sentidos
-    vehiculo->setConductor(conductor);
-    conductor->linkVehiculo(vehiculo);
-    return 0;
-}
+	int getCodigoMem();
+	string getNicknameMem();
 
-// Devuelve la información de nickname y nombre de todos los usuarios
-std::set<DTUsuario> ControladorUsuarios::listarUsuarios() {
-    std::set<DTUsuario> usuarios;
-    HandlerUsuarios* hu = HandlerUsuarios::getInstance();
-    std::map<std::string, Conductor*> conductores = hu->getConductores();
-    for (std::map<std::string, Conductor*>::iterator it = conductores.begin(); it != conductores.end(); ++it)
-        usuarios.insert(DTUsuario(it->first, it->second->getNombre(), 0));
-    std::map<std::string, Pasajero*> pasajeros = hu->getPasajeros();
-    for (std::map<std::string, Pasajero*>::iterator it = pasajeros.begin(); it != pasajeros.end(); ++it)
-        usuarios.insert(DTUsuario(it->first, it->second->getNombre(), 1));
-    return usuarios;
-}
+	static ControladorUsuarios* getInstance();
+};
 
-// Devuelve la información de viajes relacionados al usuario pasado
-std::set<DTListarViaje> ControladorUsuarios::listarViajes(std::string nickname) {
-    std::set<DTListarViaje> dtViajes;
-    Usuario* usuario = HandlerUsuarios::getInstance()->getUsuario(nickname);
-    if (usuario != nullptr) {
-        dtViajes = usuario->listarViajesUsuario();
-    }
-    this->nicknameMem = nickname;   // se guarda en memoria el nickname (caso de uso Calificar)
-    return dtViajes;
-}
-
-// Devuelve los nicknames de todos los pasajeros.
-std::set<std::string> ControladorUsuarios::listarPasajeros() {
-    std::set<std::string> pasajeros;
-    std::set<Pasajero*> listaPasajeros = HandlerUsuarios::getInstance()->listarPasajeros();
-    for (std::set<Pasajero*>::iterator it = listaPasajeros.begin(); it != listaPasajeros.end(); ++it) {
-        pasajeros.insert((*it)->getNickname());
-    }
-    return pasajeros;
-}
-
-// Genera una nueva calificación para el usuario pasado por parámetro desde el
-// usuario guardado en memoria para el viaje guardado en memoria
-//PRE: Existe en memoria el nickname del usuario calificador y el código del viaje, el valor de calificación es válido (entre 1 y 5)
-//PRE: Existe el usuario a calificar, existe una instancia de viaje con el código dado, el usuario calificado es distinto al usuario calificador, el usuario calificado participó en el viaje dado.
-bool ControladorUsuarios::calificarUsuario(std::string nicknameCalificado, int calificacion) {
-    // el codigo del viaje lo guardo CtrlViajes en su memoria (listarUsuariosViaje)
-    int codigoViaje = ControladorViajes::getInstance()->getCodigoMem();
-
-    Usuario* calificador = HandlerUsuarios::getInstance()->getUsuario(this->nicknameMem);
-    Usuario* calificado  = HandlerUsuarios::getInstance()->getUsuario(nicknameCalificado);
-
-    // si ya existe una calificacion de este calificador hacia ese usuario por ese viaje -> false
-    if (calificador->existeCalificacion(nicknameCalificado, codigoViaje)) {
-        return false;
-    }
-
-    DTFecha fechaActual = Fabrica::getInstance()->getIControladorFechaActual()->getFecha();
-
-    // el calificador crea la Calificacion (link realiza) y el calificado la recibe (link califica)
-    Calificacion* c = calificador->calificarUsuario(calificacion, fechaActual);
-    c->linkCalifica(calificado);  
-    calificado->linkCalifica(c);
-
-    // se conecta la calificacion con la Reserva del viaje correspondiente:
-    // - si el calificador es Pasajero, la reserva es la de el (recorre SUS reservas)
-    // - si el calificador es Conductor, la reserva es la del Pasajero calificado
-    if (calificador->esPasajero()) {
-        ((Pasajero*) calificador)->linkReserva(codigoViaje, c);
-    } else {
-        ((Pasajero*) calificado)->linkReserva(codigoViaje, c);
-    }
-
-    // se libera la memoria del caso de uso
-    this->nicknameMem = "";
-    this->codigoMem = 0;
-    return true;
-}
-
-std::string ControladorUsuarios::getNicknameMem() {
-    return this->nicknameMem;
-}
-
-int ControladorUsuarios::getCodigoMem() {
-    return this->codigoMem;
-}
+#endif
