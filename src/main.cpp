061d1927c11a0794c30940b952ddd62274e741dd
DevLucasPr <lucas.saracho010@gmail.com>
2026-06-14 22:25:54 -0300

    main corregido valgrind

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "../include/Menu.h"
+#include "../include/Fabrica.h"
+#include "../include/CargaDatos.h"
 
 int main() {
     Menu menu;
     menu.mostrarMenu();
+    Fabrica::destroy();      // libera controladores, handlers y objetos de dominio
+    CargaDatos::destroy();   // libera el singleton de carga de datos
     return 0;
 }
